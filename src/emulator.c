acab6ef34df3c5ae1e9ad42d979a182787725a2c
nunomota <nuno.goncalves@epfl.ch>
2015-11-25 14:25:48 +0100

    Initializes array memory to 0s with memset

--- src/emulator.c
+++ src/emulator.c
@@ -8,12 +8,17 @@ int main() {
 }
 
 void initChip() {
+    memset(chip.memory, 0, CHIP_MEMORY*sizeof(unsigned char));
+    memset(chip.v_reg, 0, V_REGISTERS*sizeof(unsigned char));
     chip.I = 0;
     chip.pc = 0;
     chip.opcode = 0;
+    memset(chip.stack, 0, STACK_SIZE*sizeof(unsigned int));
     chip.stack_ptr = 0;
     chip.delay_timer = 0;
     chip.sound_timer = 0;
+    memset(chip.keys, 0, KEYS*sizeof(unsigned char));
+    memset(chip.display, 0, DISPLAY_SIZE*sizeof(unsigned char));
     chip.display_width = DISPLAY_WIDTH;
     chip.display_height = DISPLAY_HEIGHT;
 }
