c43ca34e2f8b3003331980776841cd62b0dd01c8
nunomota <nuno.goncalves@epfl.ch>
2015-11-25 13:52:57 +0100

    Adds constants to emulator.h

---
+++ inc/emulator.h
@@ -0,0 +1,7 @@
+#define CHIP_MEMORY     4096        //in bytes
+#define V_REGISTERS     16          //total number of 'v' registers
+#define STACK_SIZE      16
+#define KEYS            16
+#define DISPLAY_WIDTH   64
+#define DISPLAY_HEIGHT  32
+#define DISPLAY_SIZE    64*32
