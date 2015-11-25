71bc1713e3095263033b22e6f9cf49fa272c66d5
nunomota <nuno.goncalves@epfl.ch>
2015-11-25 14:53:20 +0100

    Implements 0x8NNN opcodes

--- src/emulator.c
+++ src/emulator.c
@@ -32,6 +32,7 @@ void runChip(chip8* chip) {
 
     //decode opcode
     switch(chip->opcode & 0xF000) {
+        int auxiliary_var;
         case 0x0000:
             switch(chip->opcode & 0x00FF) {
                 case 0x00E0:    //00E0, clears the screen
@@ -81,6 +82,69 @@ void runChip(chip8* chip) {
         case 0x7000:        //7XNN, adds NN to VX
             chip->v_reg[(chip->opcode & 0x0F00) >> 8] += (chip->opcode & 0x00FF);
             chip->pc += 2;
+        break;
+        case 0x8000:
+            switch(chip->opcode & 0x000F) {
+                case 0x0000:        //8XY0, sets VX to the value of VY
+                    chip->v_reg[(chip->opcode & 0x0F00) >> 8] = chip->v_reg[(chip->opcode & 0x00F0) >> 4];
+                    chip->pc += 2;
+                break;
+                case 0x0001:        //8XY1, sets VX to VX or VY
+                    chip->v_reg[(chip->opcode & 0x0F00) >> 8] = chip->v_reg[(chip->opcode & 0x0F00) >> 8] | chip->v_reg[(chip->opcode & 0x00F0) >> 4];
+                    chip->pc += 2;
+                break;
+                case 0x0002:        //8XY2, sets VX to VX and VY
+                    chip->v_reg[(chip->opcode & 0x0F00) >> 8] = chip->v_reg[(chip->opcode & 0x0F00) >> 8] & chip->v_reg[(chip->opcode & 0x00F0) >> 4];
+                    chip->pc += 2;
+                break;
+                case 0x0003:        //8XY3, sets VX to VX xor VY
+                    chip->v_reg[(chip->opcode & 0x0F00) >> 8] = chip->v_reg[(chip->opcode & 0x0F00) >> 8] ^ chip->v_reg[(chip->opcode & 0x00F0) >> 4];
+                    chip->pc += 2;
+                break;
+                case 0x0004:        //8XY4, adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't
+                    auxiliary_var = (int)chip->v_reg[(chip->opcode & 0x0F00) >> 8 ] + (int)chip->v_reg[(chip->opcode & 0x00F0) >> 4];
+                    if(auxiliary_var < 256) {
+                        chip->v_reg[0xF] &= 0;
+                    } else {
+                        chip->v_reg[0xF] = 1;
+                    }
+                    chip->v_reg[(chip->opcode & 0x0F00) >> 8] = auxiliary_var & 0xF;
+                    chip->pc += 2;
+                break;
+                case 0x0005:        //8XY5, VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't
+                    auxiliary_var = (int)chip->v_reg[(chip->opcode & 0x0F00) >> 8 ] - (int)chip->v_reg[(chip->opcode & 0x00F0) >> 4];
+                    if(auxiliary_var >= 0) {
+                        chip->v_reg[0xF] = 1;
+                    } else {
+                        chip->v_reg[0xF] &= 0;
+                    }
+                    chip->v_reg[(chip->opcode & 0x0F00) >> 8] = auxiliary_var & 0xF;
+                    chip->pc += 2;
+                break;
+                case 0x0006:        //8XY6, shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift
+                    chip->v_reg[0xF] = chip->v_reg[(chip->opcode & 0x0F00) >> 8] & 7;
+                    chip->v_reg[(chip->opcode & 0x0F00) >> 8] = chip->v_reg[(chip->opcode & 0x0F00) >> 8] >> 1;
+                    chip->pc += 2;
+                break;
+                case 0x0007:        //8XY7, sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't
+                    auxiliary_var = (int)chip->v_reg[(chip->opcode & 0x00F0) >> 4] - (int)chip->v_reg[(chip->opcode & 0x0F00) >> 8];
+                    if(auxiliary_var >= 0) {
+                        chip->v_reg[0xF] = 1;
+                    } else {
+                        chip->v_reg[0xF] &= 0;
+                    }
+                    chip->v_reg[(chip->opcode & 0x0F00) >> 8] = auxiliary_var & 0xF;
+                    chip->pc += 2;
+                break;
+                case 0x000E:        //8XYE, shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift
+                    chip->v_reg[0xF] = chip->v_reg[(chip->opcode & 0x0F00) >> 8] >> 7;
+                    chip->v_reg[(chip->opcode & 0x0F00) >> 8] = chip->v_reg[(chip->opcode & 0x0F00) >> 8] << 1;
+                    chip->pc += 2;
+                break;
+                default:
+                    printf("OP code %04X is not supported...", chip->opcode);
+                break;
+            }
         break;
         default:
             printf("OP code %04X is not supported", chip->opcode);
