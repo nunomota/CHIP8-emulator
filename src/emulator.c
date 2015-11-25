2f679daabd5d6de6db42671e6907a520b42eef2f
nunomota <nuno.goncalves@epfl.ch>
2015-11-25 16:08:32 +0100

    Adds initial comment to emulator.c

--- src/emulator.c
+++ src/emulator.c
@@ -1,3 +1,28 @@
+/*
+This is a really simple implementation of a Chip8 emulator.
+
+I chose to make it an 'interpreting' and not a 'recompiling' emulator for the sake of simplicity (as a static recompiling 
+one has its downfall for self-modifying code, a dynamic one should be chosen instead - which would mean a lot of work) 
+but I might change this in the future.
+
+Implementation choices:
+   - use base size 'int' over 'short' or 'long' 
+      -> reduces amount of code the compiler generates to convert between different integer lengths;
+      -> may also reduce the memory access time, as some CPUs work fastest when reading/writing data of the base size;
+   - use 'for(;;)' instead of 'while(CPUIsRunning)' or 'while(1)'
+      -> 'while(CPUIsRunning)' can terminate the loop at any moment by setting the variable to 0, which is good,
+      but checking the variable on each pass can waste quite a lot of CPU time;
+      -> 'while(1)' is clearly a bad choice, as some compilers will generate code checking whether 1 is true or not;
+   - make memory read/write directly to an array and not through a function
+      -> this will avoid the overhead associated with a function call;
+      -> it is just possible because we are not using, for example, paged memory;
+
+Resources:
+   http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+   https://en.wikipedia.org/wiki/CHIP-8#Opcode_table
+   http://fms.komkon.org/EMUL8/HOWTO.html
+*/
+
 #include <emulator.h>
 
 int main() {
