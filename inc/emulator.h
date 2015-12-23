00f8d37d27f8c11d88c27128aa5f416a66d9640f
Cyberfox <hypernuno@hotmail.com>
2015-12-23 19:07:32 +0000

    Fixes SDL library path

--- inc/emulator.h
+++ inc/emulator.h
@@ -1,7 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <SDL/SDL.h>
+#include <SDL2/SDL.h>
 
 #define CHIP_MEMORY     4096                //in bytes
 #define V_REGISTERS     16                  //total number of 'v' registers
